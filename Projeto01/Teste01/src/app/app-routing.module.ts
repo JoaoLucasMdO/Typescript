819f4a98100aea07c2c152dfc42b43caa5894c95
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2023-11-09 20:36:23 -0300

    ATT

--- Projeto01/Teste01/src/app/app-routing.module.ts
+++ Projeto01/Teste01/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClienteListComponent } from './cliente/cliente-list/cliente-list.component';
+import { ClienteFormComponent } from './cliente/cliente-form/cliente-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'clientes', component:ClienteListComponent},
+  {path:'clientes/novo', component:ClienteFormComponent},
+  {path:'clientes/editar/:id', component:ClienteFormComponent},
+  {path:'',
+   redirectTo: '/clientes',
+   pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Projeto01/Teste01/src/app/cliente/cliente-form/cliente-form.component.ts
+++ Projeto01/Teste01/src/app/cliente/cliente-form/cliente-form.component.ts
@@ -1,31 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../shared/cliente';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-cliente-form',
   templateUrl: './cliente-form.component.html',
   styleUrls: ['./cliente-form.component.css']
 })
-export class ClienteFormComponent {
+export class ClienteFormComponent implements OnInit{
 //Criando variavel do tipo Cliente
 'cliente' : Cliente;
 
 //Criando variável vazia
 title = '';
 
-constructor(){
+constructor(private route: ActivatedRoute, private router: Router){
 }
 
 ngOnInit(){
   this.title = 'Novo Cliente';
   this.cliente = new Cliente();  
+
+  const id = this.route.snapshot.paramMap.get('id');
 }
 
 onSubmit(){
 console.log(this.cliente);
-
+this.router.navigate(['/clientes']);
 }
-
 }
 
 
